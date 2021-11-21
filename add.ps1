804e2d7e68c430d9358c1560245efa1d6d944d06
minieyes <chenvey2@gmail.com>
2021-11-21 17:07:58 +0800

    add switch for minieyes-pc2

--- add.ps1
+++ add.ps1
@@ -4,6 +4,10 @@ switch ("$env:COMPUTERNAME-$env:USERNAME")
     {
         nvr -l "D:\minieyes\bin\$($args[0]).ps1"
     }
+    "MINIEYES-PC2-chenv"
+    {
+        nvr -l "L:\bin\$($args[0]).ps1"
+    }
     default
     {
         throw No matching settings
