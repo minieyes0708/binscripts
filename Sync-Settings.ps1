8816385740000dd73e894aa1d133af2ea176c9d9
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-11-22 10:29:34 +0800

    add sync bin

--- Sync-Settings.ps1
+++ Sync-Settings.ps1
@@ -10,6 +10,10 @@ echo '===== Sync .bashrc.d ====='
 cd ~/.bashrc.d
 git push
 git pull
+echo '===== Sync bin ====='
+cd D:\minieyes_chen\software\bin
+git push
+git pull
 echo '===== Sync Algorithms ====='
 cd D:\minieyes_chen\program\learning\java\Algorithms\
 git push
