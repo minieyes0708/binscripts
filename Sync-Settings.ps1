5a0e69e2871cb07fa8165a3ca6cb7044a0637887
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-11-22 10:29:01 +0800

    add Sync-Settings.ps1

---
+++ Sync-Settings.ps1
@@ -0,0 +1,16 @@
+echo '===== Sync .vimrc.d ====='
+cd ~/.vimrc.d
+git push
+git pull
+echo '===== Sync .vifm ====='
+cd ~/.vifm
+git push
+git pull
+echo '===== Sync .bashrc.d ====='
+cd ~/.bashrc.d
+git push
+git pull
+echo '===== Sync Algorithms ====='
+cd D:\minieyes_chen\program\learning\java\Algorithms\
+git push
+git pull
