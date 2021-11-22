44536b91277145fc5ee0a544ee626db98c4fabef
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-11-22 10:22:05 +0800

    add settings for novatek

--- add.ps1
+++ add.ps1
@@ -4,6 +4,10 @@ switch ("$env:COMPUTERNAME-$env:USERNAME")
     {
         nvr -l "D:\minieyes\bin\$($args[0]).ps1"
     }
+    "NVT02863_NB-NVT02863"
+    {
+        nvr -l "D:\minieyes_chen\software\bin\$($args[0]).ps1"
+    }
     default
     {
         throw No matching settings
