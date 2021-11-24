87c1640f03f05eb1f7c73c8c5f4b429249af7b48
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-11-24 09:03:40 +0800

    should pull before push

--- Sync-Settings.ps1
+++ Sync-Settings.ps1
@@ -1,20 +1,20 @@
 echo '===== Sync .vimrc.d ====='
 cd D:/minieyes_chen/software/Neovim/share/nvim/.vimrc.d
-git push
 git pull
+git push
 echo '===== Sync .vifm ====='
 cd ~/.vifm
-git push
 git pull
+git push
 echo '===== Sync .bashrc.d ====='
 cd ~/.bashrc.d
-git push
 git pull
+git push
 echo '===== Sync bin ====='
 cd D:\minieyes_chen\software\bin
-git push
 git pull
+git push
 echo '===== Sync Algorithms ====='
 cd D:\minieyes_chen\program\learning\java\Algorithms\
-git push
 git pull
+git push
