b86fcf262209e60b5b057da1abce4d6ab2dc8a1b
Minieyes Chen <Minieyes_Chen@novatek.com.tw>
2021-11-24 09:23:31 +0800

    if is D:/minieyes_chen/program, search deeper one level

--- Select-Bookmark.ps1
+++ Select-Bookmark.ps1
@@ -1,5 +1,15 @@
 $target_path = $(Get-Content ~/.vifm/user/bookmarks.txt | fzf)
 
+switch ("$env:COMPUTERNAME-$env:USERNAME")
+{
+    "NVT02863_NB-NVT02863"
+    {
+        if ($target_path -eq "D:/minieyes_chen/program") {
+            $target_path = $(fd -a -i -d 1 -t d . $target_path | fzf)
+        }
+    }
+}
+
 if ($target_path) {
     echo $target_path
 } else {
