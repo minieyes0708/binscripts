558de4a35a423557e9047ad9f28d553ac63c3cdc
minieyes <chenvey2@gmail.com>
2021-12-11 22:44:36 +0800

    x.ps1 sync with novatek

--- x.ps1
+++ x.ps1
@@ -1,34 +1,34 @@
-$options = @(
-    "git log",`
-    "git diff",`
-    "git pull",`
-    "git push",`
-    "git blame",`
-    "git commit",`
-    "git revert",`
-    "git log on current file",`
-    "git diff with prev version",`
+$options = (
+    "git log",
+    "git diff",
+    "git pull",
+    "git push",
+    "git blame",
+    "git commit",
+    "git revert",
+    "git log on current file",
+    "git diff with prev version",
 
-    "svn log",`
-    "svn diff",`
-    "svn pull",`
-    "svn update",`
+    "svn log",
+    "svn diff",
+    "svn pull",
+    "svn update",
     "svn commit")
 
-switch ($($options | fzf)) {
-    "git log"                    { TortoiseGitProc.exe -command log }
-    "git diff"                   { TortoiseGitProc.exe -command diff }
-    "git pull"                   { TortoiseGitProc.exe -command pull }
-    "git push"                   { TortoiseGitProc.exe -command push }
-    "git blame"                  { TortoiseGitProc.exe -command blame -path $args }
-    "git commit"                 { TortoiseGitProc.exe -command commit }
-    "git revert"                 { TortoiseGitProc.exe -command revert }
-    "git log on current file"    { TortoiseGitProc.exe -command log -path $args }
+switch ($($options -join "`n" | fzf)) {
+    "git log"                    { TortoiseGitProc.exe -path . -command log }
+    "git diff"                   { TortoiseGitProc.exe -path . -command diff }
+    "git pull"                   { TortoiseGitProc.exe -path . -command pull }
+    "git push"                   { TortoiseGitProc.exe -path . -command push }
+    "git blame"                  { TortoiseGitProc.exe -path $($args[0]) -command blame }
+    "git commit"                 { TortoiseGitProc.exe -path . -command commit }
+    "git revert"                 { TortoiseGitProc.exe -path . -command revert }
+    "git log on current file"    { TortoiseGitProc.exe -path $($args[0]) -command log }
     "git diff with prev version" { TortoiseGitProc.exe -command showcompare -revision1 HEAD^ -revision2 HEAD }
 
-    "svn log"                    { TortoiseProc.exe -path "." -command:log }
-    "svn diff"                   { TortoiseProc.exe -path "." -command:diff }
-    "svn pull"                   { TortoiseProc.exe -path "." -command:pull }
-    "svn update"                 { TortoiseProc.exe -path "." -command:update }
-    "svn commit"                 { TortoiseProc.exe -path "." -command:commit }
+    "svn log"                 { TortoiseProc.exe -path . -command:log }
+    "svn diff"                { TortoiseProc.exe -path . -command:diff }
+    "svn pull"                { TortoiseProc.exe -path . -command:pull }
+    "svn update"              { TortoiseProc.exe -path . -command:update }
+    "svn commit"              { TortoiseProc.exe -path . -command:commit }
 }
