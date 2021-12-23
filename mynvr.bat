eba7c1f7dfc51bbef3424c8c967ea4b5c75e4247
minieyes <chenvey2@gmail.com>
2021-12-23 22:31:35 +0800

    fix if syntax

--- mynvr.bat
+++ mynvr.bat
@@ -1,14 +1,14 @@
 @SET FULL_NAME=%COMPUTERNAME%-%USERNAME%
 
-@IF %FULL_NAME% == MINIEYESNB1-怡哲 DO @(
+@IF %FULL_NAME% == MINIEYESNB1-怡哲 (
     @py -m tools.mynvr "%*"
     @GOTO :END
 )
-@IF %FULL_NAME% == NVT02863_NB-NVT02863 DO @(
+@IF %FULL_NAME% == NVT02863_NB-NVT02863 (
     @py -m miniLibrary.mynvr "%*"
     @GOTO :END
 )
-@IF %FULL_NAME% == MINIEYES-PC2-chenv DO @(
+@IF %FULL_NAME% == MINIEYES-PC2-chenv (
     @py -m tools.mynvr "%*"
     @GOTO :END
 )
