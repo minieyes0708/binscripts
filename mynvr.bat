271be48007f4bb8880d9c2388367db0216544250
minieyes <chenvey2@gmail.com>
2021-12-23 22:52:21 +0800

    mynvr.py now only echo servername

--- mynvr.bat
+++ mynvr.bat
@@ -1,15 +1,17 @@
 @SET FULL_NAME=%COMPUTERNAME%-%USERNAME%
 
+@FOR /F "delims=" %%i IN ('py -m tools.mynvr') DO @SET SERVERNAME=%%i
+
 @IF %FULL_NAME% == MINIEYESNB1-怡哲 (
-    @py -m tools.mynvr "%*"
+    @nvr --servername %SERVERNAME% "%*"
     @GOTO :END
 )
 @IF %FULL_NAME% == NVT02863_NB-NVT02863 (
-    @py -m miniLibrary.mynvr "%*"
+    @nvr --servername %SERVERNAME% "%*"
     @GOTO :END
 )
 @IF %FULL_NAME% == MINIEYES-PC2-chenv (
-    @py -m tools.mynvr "%*"
+    @nvr --servername %SERVERNAME% "%*"
     @GOTO :END
 )
 @ECHO No matching settings
