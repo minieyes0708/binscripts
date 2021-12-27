949deac2eb592858b725c690ffc7886c2a566346
minieyes <chenvey2@gmail.com>
2021-12-27 23:12:28 +0800

    add Select-Bookmark.bat and Sync-Settings.ps1 from novatek

---
+++ Sync-Settings.ps1
@@ -0,0 +1,20 @@
+echo '===== Sync .vimrc.d ====='
+cd D:/minieyes_chen/software/Neovim/share/nvim/.vimrc.d
+git pull
+git push
+echo '===== Sync .vifm ====='
+cd ~/.vifm
+git pull
+git push
+echo '===== Sync .bashrc.d ====='
+cd ~/.bashrc.d
+git pull
+git push
+echo '===== Sync bin ====='
+cd D:\minieyes_chen\software\bin
+git pull
+git push
+echo '===== Sync Algorithms ====='
+cd D:\minieyes_chen\program\learning\java\Algorithms\
+git pull
+git push
