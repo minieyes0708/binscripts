e6388c32796c553e5a95ee73ecfd5028ea114a39
Minieyes Chen(陳怡哲) <Minieyes_Chen@novatek.com.tw>
2021-12-29 11:23:03 +0800

    cannot set var in nested for in switch

--- mynvr.bat
+++ mynvr.bat
@@ -1,19 +1,22 @@
 @SET FULL_NAME=%COMPUTERNAME%-%USERNAME%
 
 @IF %FULL_NAME% == MINIEYESNB1-怡哲 (
-    @FOR /F "delims=" %%i IN ('py -m tools.mynvr') DO @SET SERVERNAME=%%i
-    @nvr --servername %SERVERNAME% %*
-    @GOTO :END
+    @FOR /F "delims=" %%i IN ('py -m tools.mynvr') DO @(
+        @nvr --servername %%i %*
+        @GOTO :END
+    )
 )
 @IF %FULL_NAME% == NVT02863_NB-NVT02863 (
-    @FOR /F "delims=" %%i IN ('py -m miniLibrary.mynvr') DO @SET SERVERNAME=%%i
-    @nvr --servername %SERVERNAME% %*
-    @GOTO :END
+    @FOR /F "delims=" %%i IN ('py -m miniLibrary.mynvr') DO @(
+        @nvr --servername %%i %*
+        @GOTO :END
+    )
 )
 @IF %FULL_NAME% == MINIEYES-PC2-chenv (
-    @FOR /F "delims=" %%i IN ('py -m tools.mynvr') DO @SET SERVERNAME=%%i
-    @nvr --servername %SERVERNAME% %*
-    @GOTO :END
+    @FOR /F "delims=" %%i IN ('py -m tools.mynvr') DO @(
+        @nvr --servername %%i %*
+        @GOTO :END
+    )
 )
 @ECHO No matching settings
 
