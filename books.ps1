fb21973c088cfa2bf1ce238148b7f19db04f9b67
Minieyes Chen <chenvey2@gmail.com>
2023-08-14 21:20:16 +0800

    fix Set-Process to Start-Process

--- books.ps1
+++ books.ps1
@@ -1,4 +1,4 @@
 . "$($env:DotConfigPath)/powershell/settings.ps1"
 
 $option = $(fd --base-directory $BooksFolderPath | fzf) -replace '\\','/'
-if ($option) { Set-Process "$BooksFolderPath/$option" }
+if ($option) { Start-Process "$BooksFolderPath/$option" }
