285a816ff106da64c1971619d2a360a0afc9f4cd
Minieyes Chen <chenvey2@gmail.com>
2024-06-21 22:02:54 +0800

    reformat x.ps1, add add bookmark

--- x.ps1
+++ x.ps1
@@ -1,34 +1,20 @@
-$options = (
-    "git log",
-    "git diff",
-    "git pull",
-    "git push",
-    "git blame",
-    "git commit",
-    "git revert",
-    "git log on current file",
-    "git diff with prev version",
+$commands = @{
+    "git log"                    = { TortoiseGitProc.exe -path . -command log }
+    "git diff"                   = { TortoiseGitProc.exe -path . -command diff }
+    "git pull"                   = { TortoiseGitProc.exe -path . -command pull }
+    "git push"                   = { TortoiseGitProc.exe -path . -command push }
+    "git blame"                  = { TortoiseGitProc.exe -path $($args[0]) -command blame }
+    "git commit"                 = { TortoiseGitProc.exe -path . -command commit }
+    "git revert"                 = { TortoiseGitProc.exe -path . -command revert }
+    "git log on current file"    = { TortoiseGitProc.exe -path $($args[0]) -command log }
+    "git diff with prev version" = { TortoiseGitProc.exe -command showcompare -revision1 HEAD^ -revision2 HEAD }
 
-    "svn log",
-    "svn diff",
-    "svn pull",
-    "svn update",
-    "svn commit")
+    "svn log"                    = { TortoiseProc.exe -path . -command:log }
+    "svn diff"                   = { TortoiseProc.exe -path . -command:diff }
+    "svn pull"                   = { TortoiseProc.exe -path . -command:pull }
+    "svn update"                 = { TortoiseProc.exe -path . -command:update }
+    "svn commit"                 = { TortoiseProc.exe -path . -command:commit }
 
-switch ($($options -join "`n" | fzf)) {
-    "git log"                    { TortoiseGitProc.exe -path . -command log }
-    "git diff"                   { TortoiseGitProc.exe -path . -command diff }
-    "git pull"                   { TortoiseGitProc.exe -path . -command pull }
-    "git push"                   { TortoiseGitProc.exe -path . -command push }
-    "git blame"                  { TortoiseGitProc.exe -path $($args[0]) -command blame }
-    "git commit"                 { TortoiseGitProc.exe -path . -command commit }
-    "git revert"                 { TortoiseGitProc.exe -path . -command revert }
-    "git log on current file"    { TortoiseGitProc.exe -path $($args[0]) -command log }
-    "git diff with prev version" { TortoiseGitProc.exe -command showcompare -revision1 HEAD^ -revision2 HEAD }
-
-    "svn log"                 { TortoiseProc.exe -path . -command:log }
-    "svn diff"                { TortoiseProc.exe -path . -command:diff }
-    "svn pull"                { TortoiseProc.exe -path . -command:pull }
-    "svn update"              { TortoiseProc.exe -path . -command:update }
-    "svn commit"              { TortoiseProc.exe -path . -command:commit }
+    "add bookmark"               = { "`n" + (get-location).path | out-file -append $env:dotconfig\bookmarks.txt }
 }
+$commands[($commands.Keys -join "`n" | fzf)].Invoke()
