f08fe34d5adde2f99de8921abd5cd82e19f5fd8f
Minieyes Chen <chenvey2@gmail.com>
2024-06-21 22:52:23 +0800

    remove books, overlapping functionality with start-file

--- books.ps1
+++
@@ -1,4 +0,0 @@
-. "$($env:DotConfigPath)/powershell/settings.ps1"
-
-$option = $(fd --base-directory $BooksFolderPath | fzf) -replace '\\','/'
-if ($option) { Start-Process "$BooksFolderPath/$option" }
