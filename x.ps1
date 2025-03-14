8abd56dcb67ae3712cb776532d879b1080b142e1
minieyes <chenvey2@gmail.com>
2025-03-14 22:08:26 +0800

    style fzf

--- x.ps1
+++ x.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
 $xpath = Join-Path $env:BinScriptPath 'x'
-$filename = Join-Path $xpath $((Get-ChildItem $xpath).Name | fzf)
+$filename = Join-Path $xpath $((Get-ChildItem $xpath).Name | fzf --height=40% --border --margin=5,10 --preview="bat --color=always $($env:BinScriptPath)/x/{}")
 
 & "$filename"
